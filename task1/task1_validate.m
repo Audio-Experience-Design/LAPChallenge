07a232d30fae106bd4bc0a9665e21f29b089a0e3
Roberto Barumerli <roberto@robertobarumerli.com>
2024-06-17 17:32:43 +0200

    increasing thresholds and minor adjustments

--- task1/task1_validate.m
+++ task1/task1_validate.m
@@ -9,6 +9,8 @@
 %
 % Changelog:
 % 2024-05-29: fixed bug in sampling rate and updated threshold values
+% 2024-06-17: increased threshold values, added check to SOFA distance and
+%               added absolute function to metric difference computation.
 
 clearvars 
 close all
@@ -25,6 +27,10 @@ hrtf_harmonized = 'harmonised_hrtf.sofa';
 
 sofa_original = SOFAload(hrtf_original);
 sofa_harmonized = SOFAload(hrtf_harmonized);
+
+% select HRIRs with farthest distance (e.g. SCUT collection)
+sofa_original = extract_only_farthest_distance(sofa_original); 
+sofa_harmonized = extract_only_farthest_distance(sofa_harmonized);
 
 % extract features for template and target
 template = barumerli2023_featureextraction(sofa_original, 'dtf', 'fs', sofa_original.Data.SamplingRate);
@@ -69,15 +75,15 @@ metrics_harmonized.gainP = localizationerror(m, 'gainP');
 metrics_harmonized.accP = localizationerror(m, 'accPnoquerr');
 
 % compute difference 
-metrics_diff = structfun(@(x) x, metrics_harmonized) - ...
-                        structfun(@(x) x, metrics_original);
+metrics_diff = abs(structfun(@(x) x, metrics_harmonized) - ...
+                        structfun(@(x) x, metrics_original));
 
 assert(sum(isnan(metrics_diff)) == 0, 'nan detected')
 
 % see task details to know more about these values
 % the array is ordered as follows: accL, rmsL, accP, rmsP, querr, gainP
 metrics_names = {'accL', 'rmsL', 'accP', 'rmsP', 'querr', 'gainP'};
-thresholds = [1.69, 2.35, 3.73, 3.12, 6.23, 0.15]';
+thresholds = [5.86, 20.71, 12.67, 5.90, 34.56, 0.33]';
 
 % print results
 checks = metrics_diff < thresholds;
@@ -91,7 +97,25 @@ end
 fprintf('Names:\t\t%s\n', sprintf('%s\t', metrics_names{:}))
 fprintf('Original:\t%s\n', sprintf('%.2f\t', structfun(@(x) x, metrics_original)))
 fprintf('Harmonized:\t%s\n', sprintf('%.2f\t', structfun(@(x) x, metrics_harmonized)))
+fprintf('----------------------------------------------------------------\n')
 fprintf('Difference:\t%s\n', sprintf('%.2f\t', metrics_diff))
-fprintf('Pass?:\t\t%s\n', sprintf('%i\t', checks))
+fprintf('Threshold:\t%s\n', sprintf('%.2f\t', thresholds))
+fprintf('Pass?:\t\t%s\n', sprintf('%i\t\t', checks))
 
+% This function checks if the provided SOFA file includes 
+% measurements taken at different distances and selects only the farthest ones.
+function sofa_output = extract_only_farthest_distance(sofa_input)
+    sofa_output = sofa_input;
 
+    % test presence multiple distances
+    if length(unique(round(sofa_output.SourcePosition(:, 3), 2))) > 1
+        % select maximum distance
+        far_dist = max(sofa_output.SourcePosition(:, 3));
+    
+        % remove HRIRs by updating sofa object
+        idx_remove = sofa_output.SourcePosition(:, 3) ~= far_dist;
+        sofa_output.SourcePosition(idx_remove, :) = [];
+        sofa_output.Data.IR(idx_remove, :, :) = []; 
+        sofa_output.API.M = size(sofa_output.SourcePosition, 1);
+    end
+end
