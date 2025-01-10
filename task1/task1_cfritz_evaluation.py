d32ba783ec062c6f6bda3dea9fb93a130089807b
Roberto Barumerli <robaru@users.noreply.github.com>
2025-01-10 10:23:13 +0100

    task1: added evaluation on cfritz grid

--- task1/task1_cfritz_evaluation.py
+++ task1/task1_cfritz_evaluation.py
@@ -11,18 +11,22 @@ import numpy as np
 
 
 ## Config
-path = "C:/repos/datasets/LAP/task1/IOA3D"
+path = "Bahu"
 
 # load common positions across collections
 # common_positions = chain(product(range(-180, 180, 10), [-30, 0, 30]), product(range(-180, 180, 20), [60]))
-common_positions = chain([tuple(row) for row in np.loadtxt('task1/evaluation_coords.csv', delimiter=',') if len(row) == 2])
+common_positions = chain([tuple(row) for row in np.loadtxt('task1/cfritz_evaluation_coords.csv', delimiter=',') if len(row) == 2])
 
 side = 'any-left'
 sample_rate = 44100
 hrir_length = 235
 rseed = 1024
 
-accuracy, stdev = classification_accuracy(Path(path), rseed, common_positions, side, sample_rate, hrir_length)
+mean_acc, std_acc = classification_accuracy(Path(path), rseed, common_positions, side, sample_rate, hrir_length)
 
 # print results 
-print("%.4f stdev %.4f" % accuracy, stdev)
+print("%.4f stdev %.4f" % (mean_acc, std_acc))
+
+# IOA3D 0.325 stdev 0.0318
+# Arevalo 0.9500 stdev 0.0468
+# Bahu 0.8938 stdev 0.0897
